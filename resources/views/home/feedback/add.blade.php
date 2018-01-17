a596c929b264c233825a4fe63b3e623abccad43c
messikiller <messikiller@aliyun.com>
2018-01-17 17:50:58 +0800

    fix

--- resources/views/home/feedback/add.blade.php
+++ resources/views/home/feedback/add.blade.php
@@ -65,7 +65,7 @@ new Vue({
                 scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
                 success: function (res) {
                    // 回调
-                }
+                },
                 error: function(res){
                       if(res.errMsg.indexOf('function_not_exist') > 0){
                            alert('版本过低请升级')
