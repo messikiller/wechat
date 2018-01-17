7516dcdc84dfc48e4fb9e75726ef7bcada8c862e
messikiller <messikiller@aliyun.com>
2018-01-17 17:47:23 +0800

    fix

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <title>SonoScape</title>
 <link rel="stylesheet" href="{{ mix('css/home.css') }}">
 <style media="screen">
