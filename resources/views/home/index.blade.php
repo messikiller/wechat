3e3bd4f70aaa32d04792a793b428a638d5e8e6cc
messikiller <messikiller@aliyun.com>
2018-01-18 17:51:51 +0800

    fix

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -26,7 +26,7 @@
             <i class="icon ion-chatbubble-working primary-color" style="font-size: 24px;"></i>
         </div>
         <p class="weui-grid__label">
-            我的反馈
+            我的报修
         </p>
     </a>
     <a href="javascript:;" class="weui-grid">
@@ -37,17 +37,12 @@
             系统设置
         </p>
     </a>
-</div>
-
-<div class="bar back-gray">功能</div>
-<div class="weui-grids">
-
     <a href={{ route('feedback.add') }} class="weui-grid">
         <div class="weui-grid__icon text-center">
             <i class="icon ion-chatbox-working primary-color" style="font-size: 24px;"></i>
         </div>
         <p class="weui-grid__label">
-        提交反馈
+            设备报修
         </p>
     </a>
     <a href="javascript:;" class="weui-grid">
@@ -67,4 +62,81 @@
         </p>
     </a>
 </div>
+
+<div class="bar back-gray">维修中心</div>
+<div class="weui-grids">
+    <a href="javascript:;" class="weui-grid">
+        <div class="weui-grid__icon text-center">
+            <i class="icon ion-android-settings primary-color" style="font-size: 24px;"></i>
+        </div>
+        <p class="weui-grid__label">
+            医生中心（ES）
+        </p>
+    </a>
+    <a href="javascript:;" class="weui-grid">
+        <div class="weui-grid__icon text-center">
+            <i class="icon ion-android-settings primary-color" style="font-size: 24px;"></i>
+        </div>
+        <p class="weui-grid__label">
+            医生中心（US）
+        </p>
+    </a>
+    <a href="javascript:;" class="weui-grid">
+        <div class="weui-grid__icon text-center">
+            <i class="icon ion-android-settings primary-color" style="font-size: 24px;"></i>
+        </div>
+        <p class="weui-grid__label">
+            经销商中心
+        </p>
+    </a>
+</div>
+
+<div class="bar back-gray">技术支持</div>
+<div class="weui-grids">
+    <a href="javascript:;" class="weui-grid">
+        <div class="weui-grid__icon text-center">
+            <i class="icon ion-ios-paper primary-color" style="font-size: 24px;"></i>
+        </div>
+        <p class="weui-grid__label">
+            新闻
+        </p>
+    </a>
+    <a href="javascript:;" class="weui-grid">
+        <div class="weui-grid__icon text-center">
+            <i class="icon ion-android-settings primary-color" style="font-size: 24px;"></i>
+        </div>
+        <p class="weui-grid__label">
+            超声
+        </p>
+    </a>
+    <a href="javascript:;" class="weui-grid">
+        <div class="weui-grid__icon text-center">
+            <i class="icon ion-android-settings primary-color" style="font-size: 24px;"></i>
+        </div>
+        <p class="weui-grid__label">
+            内窥镜
+        </p>
+    </a>
+</div>
+
+<div class="bar back-gray">关于开立</div>
+<div class="weui-grids">
+    <a href="javascript:;" class="weui-grid">
+        <div class="weui-grid__icon text-center">
+            <i class="icon ion-android-contacts primary-color" style="font-size: 24px;"></i>
+        </div>
+        <p class="weui-grid__label">
+            关于我们
+        </p>
+    </a>
+    <a href="javascript:;" class="weui-grid">
+        <div class="weui-grid__icon text-center">
+            <i class="icon ion-earth primary-color" style="font-size: 24px;"></i>
+        </div>
+        <p class="weui-grid__label">
+            全球分支
+        </p>
+    </a>
+</div>
+
 @endsection
