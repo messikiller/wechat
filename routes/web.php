97df8a4c700df5cc9ea444d3f32ccc998f27d761
messikiller <messikiller@aliyun.com>
2018-01-18 10:51:19 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ Route::namespace('Home')->middleware('wechat')->group(function () {
     Route::get('/feedback/add',   'FeedbackController@add')->name('feedback.add');
     Route::get('/feedback/of/me', 'FeedbackController@ofMe')->name('feedback.ofMe');
 
-    Route::get('customer/service/list', 'CustomerService@list')->name('customerService.list');
-    Route::get('customer/service/open', 'CustomerService@open')->name('customerService.open');
+    Route::get('customer/service/list', 'CustomerServiceController@list')->name('customerService.list');
+    Route::get('customer/service/open', 'CustomerServiceController@open')->name('customerService.open');
 
     Route::get('/common/success', function () {
         return view('home.common.message', [
