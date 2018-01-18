738a88c344578dd5eb035a53370fc21fea710d31
messikiller <messikiller@aliyun.com>
2018-01-18 10:52:43 +0800

    fix

--- resources/views/home/customer_service/list.blade.php
+++ resources/views/home/customer_service/list.blade.php
@@ -4,7 +4,7 @@
 
     <div class="weui-flex">
         @foreach ($serviceList as $service)
-            <div class="weui-flex__item"><a href="{{ route('customerService.open', ['open_id' => $service['kf_account'])] }}">{{ $service['kf_nick'] }}</a></div>
+            <div class="weui-flex__item"><a href="{{ route('customerService.open', ['open_id' => $service['kf_account']]) }}">{{ $service['kf_nick'] }}</a></div>
         @endforeach
     </div>
 
