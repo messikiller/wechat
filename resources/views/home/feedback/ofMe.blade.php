2a6d47d5b71f7b2f75381511440a1a7e19b3839e
messikiller <messikiller@aliyun.com>
2018-01-23 18:03:33 +0800

    fix

--- resources/views/home/feedback/ofMe.blade.php
+++ resources/views/home/feedback/ofMe.blade.php
@@ -1,43 +1,65 @@
 @extends('layouts.home')
 
 @section('content')
-    <div class="weui-cells__title">我的反馈信息</div>
-    <div class="weui-cells">
-        <a class="weui-cell weui-cell_access" href="javascript:;">
-            <div class="weui-cell__bd">
-                <p>SN:453434346778</p>
-            </div>
-            <div class="weui-cell__ft">2017-09-28</div>
-        </a>
-        <a class="weui-cell weui-cell_access" href="javascript:;">
-            <div class="weui-cell__bd">
-                <p>SN:453434346778</p>
-            </div>
-            <div class="weui-cell__ft">2017-09-28</div>
-        </a>
-        <a class="weui-cell weui-cell_access" href="javascript:;">
-            <div class="weui-cell__bd">
-                <p>SN:453434346778</p>
-            </div>
-            <div class="weui-cell__ft">2017-09-28</div>
-        </a>
-        <a class="weui-cell weui-cell_access" href="javascript:;">
-            <div class="weui-cell__bd">
-                <p>SN:453434346778</p>
-            </div>
-            <div class="weui-cell__ft">2017-09-28</div>
-        </a>
-        <a class="weui-cell weui-cell_access" href="javascript:;">
-            <div class="weui-cell__bd">
-                <p>SN:453434346778</p>
-            </div>
-            <div class="weui-cell__ft">2017-09-28</div>
-        </a>
-        <a class="weui-cell weui-cell_access" href="javascript:;">
-            <div class="weui-cell__bd">
-                <p>SN:453434346778</p>
-            </div>
-            <div class="weui-cell__ft">2017-09-28</div>
-        </a>
+
+<div class="weui-tab">
+    <div class="weui-navbar">
+        <div class="weui-navbar__item weui-bar__item_on">
+            选项一
+        </div>
+        <div class="weui-navbar__item">
+            选项二
+        </div>
+        <div class="weui-navbar__item">
+            选项三
+        </div>
     </div>
+    <div class="weui-tab__panel">
+        <div>
+            <div class="weui-cells__title">我的反馈信息</div>
+            <div class="weui-cells">
+                <a class="weui-cell weui-cell_access" href="javascript:;">
+                    <div class="weui-cell__bd">
+                        <p>SN:453434346778</p>
+                    </div>
+                    <div class="weui-cell__ft">2017-09-28</div>
+                </a>
+                <a class="weui-cell weui-cell_access" href="javascript:;">
+                    <div class="weui-cell__bd">
+                        <p>SN:453434346778</p>
+                    </div>
+                    <div class="weui-cell__ft">2017-09-28</div>
+                </a>
+                <a class="weui-cell weui-cell_access" href="javascript:;">
+                    <div class="weui-cell__bd">
+                        <p>SN:453434346778</p>
+                    </div>
+                    <div class="weui-cell__ft">2017-09-28</div>
+                </a>
+                <a class="weui-cell weui-cell_access" href="javascript:;">
+                    <div class="weui-cell__bd">
+                        <p>SN:453434346778</p>
+                    </div>
+                    <div class="weui-cell__ft">2017-09-28</div>
+                </a>
+                <a class="weui-cell weui-cell_access" href="javascript:;">
+                    <div class="weui-cell__bd">
+                        <p>SN:453434346778</p>
+                    </div>
+                    <div class="weui-cell__ft">2017-09-28</div>
+                </a>
+                <a class="weui-cell weui-cell_access" href="javascript:;">
+                    <div class="weui-cell__bd">
+                        <p>SN:453434346778</p>
+                    </div>
+                    <div class="weui-cell__ft">2017-09-28</div>
+                </a>
+            </div>
+        </div>
+        <div style="display:none">Page 2</div>
+        <div style="display:none">Page 3</div>
+    </div>
+</div>
+
+
 @endsection
