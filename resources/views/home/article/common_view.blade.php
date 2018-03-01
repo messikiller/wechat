c96f210ce95e62dc94992a487d24af33e7e51266
messikiller <messikiller@aliyun.com>
2018-03-01 11:56:46 +0800

    fix article share

--- resources/views/home/article/common_view.blade.php
+++ resources/views/home/article/common_view.blade.php
@@ -15,24 +15,25 @@ new Vue({
     el: '#app',
     created: function () {
         wx.config({!! $wx_config !!});
+        wx.ready(() => {
+            wx.onMenuShareTimeline({
+                title: '{{ $article->title }}',
+                link: '{{ url()->current() }}',
+                imgUrl: '{{ asset($article->cover) }}',
+                success: function () {},
+                cancel: function () {}
+            });
 
-        wx.onMenuShareTimeline({
-            title: '{{ $article->title }}',
-            link: '{{ url()->current() }}',
-            imgUrl: '{{ asset($article->cover) }}',
-            success: function () {},
-            cancel: function () {}
-        });
-
-        wx.onMenuShareAppMessage({
-            title: '{{ $article->title }}',
-            desc: '{{ $article->abstract }}',
-            link: '{{ url()->current() }}',
-            imgUrl: '{{ asset($article->cover) }}',
-            type: 'link',
-            data: '',
-            success: function () {},
-            cancel: function () {}
+            wx.onMenuShareAppMessage({
+                title: '{{ $article->title }}',
+                desc: '{{ $article->abstract }}',
+                link: '{{ url()->current() }}',
+                imgUrl: '{{ asset($article->cover) }}',
+                type: 'link',
+                data: '',
+                success: function () {},
+                cancel: function () {}
+            });
         });
     },
     data: {
