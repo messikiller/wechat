6b23c2c7406ae1539fcade49f19b49401d44983d
messikiller <messikiller@aliyun.com>
2018-03-06 14:18:57 +0800

    supermen list added

--- config/supermen.php
+++ config/supermen.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * people who can be through middleware checkMemberProfile
+ * people whose openid can be through middleware checkMemberProfile
  *
  * @see checkMemberProfile
  */
 return [
+    'o6Mrxs-XBdoJSCtlVNB8JhrSQQdA', //Wang Zheng
+    'o6MrxszaHFYCMeYfNoloqlJ6oCUw', //Gao Cheng
     'o6Mrxs7_afGd5f1ujczg7HCu_KXg', //Json Huang
 ];
