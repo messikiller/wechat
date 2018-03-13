7d43f0d13fc06c1efba34830f163c71f7fbb2be3
messikiller <messikiller@aliyun.com>
2018-03-13 14:21:19 +0800

    add superman

--- config/supermen.php
+++ config/supermen.php
@@ -8,4 +8,5 @@ return [
     'o6Mrxs-XBdoJSCtlVNB8JhrSQQdA', //Wang Zheng
     'o6MrxszaHFYCMeYfNoloqlJ6oCUw', //Gao Cheng
     'o6Mrxs7_afGd5f1ujczg7HCu_KXg', //Json Huang
+    'o6Mrxs7EsslUMbqvqEHJKQfXTfZ4', //Pimkie
 ];
